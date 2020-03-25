9342ce2e46282a9e363151723935636d9d3f796c
Muqiet09 <46033477+Muqiet09@users.noreply.github.com>
2020-03-25 23:06:00 +0500

    Database File

---
+++ SQLQuery4.sql
@@ -0,0 +1,10 @@
+use MVCEmployees
+create table Employees(
+	EmployeeId int,
+	LastName varchar(50),
+	FirstName varchar(50),
+	Salary int,
+	Gender varchar(50),
+	HiredDate Date)
+	select * from Employees
+	
